53c78d2b39f7348e0607f3a6189ba481e2185d5a
macintario <macintario@gmail.com>
2018-10-27 15:48:19 -0500

    inicial

--- py-spark-hadoop.py
+++ py-spark-hadoop.py
@@ -2,7 +2,7 @@ import findspark
 findspark.init()
 import pyspark
 sc = pyspark.SparkContext(appName = "Practica")
-raw_content = sc.textFile("hdfs://localhost:50010/user/input/file1.txt")
+raw_content = sc.textFile("hdfs:///user/input/file1.txt")
 raw_content.count()
 raw_content.saveAsTextFile("hdfs:///user/output")
 sc.parallelize(raw_content.take(5)).saveAsTextFile("hdfs:///user/output2")
